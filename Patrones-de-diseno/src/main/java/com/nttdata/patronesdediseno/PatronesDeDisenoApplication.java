589d1771570c5af8461018ee342be15ac653bbd6
OscarBayoRequenaNTT <116019168+OscarBayoRequenaNTT@users.noreply.github.com>
2022-10-25 10:15:23 +0200

    Primer Push dia 2

---
+++ Patrones-de-diseno/src/main/java/com/nttdata/patronesdediseno/PatronesDeDisenoApplication.java
@@ -0,0 +1,20 @@
+package com.nttdata.patronesdediseno;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatronesDeDisenoApplication  implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatronesDeDisenoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
